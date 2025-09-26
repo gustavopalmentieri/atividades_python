84a473475b298832bb243c90fccb24cb6966bc13
Gustavo Jesus Palmentieri <gustavoo.palmentieri@gmail.com>
2025-09-25 21:21:30 -0300

    Add files via upload

---
+++ ajuda_ex8.py
@@ -0,0 +1,13 @@
+dic1 ={"nome" : "TV",
+        "preço": 2500}
+
+dic2 ={"nome" : "microondas",
+        "preço": 900}
+
+produtos = []
+
+produtos.append(dic1)
+produtos.append(dic2)
+
+for produto in produtos:
+        produto = ["nome"]
--- anotação.py
+++ anotação.py
@@ -1,3 +1,7 @@
 
 sum(saques) #soma de todos os valores
             #dentro dos parenteses a lista
+
+texto.strip() #remove os espaços da esquerda e da direita
+
+texto.replace() #retira oq for colocado dentro dela
